8a959734ed82c9a8cc3a027e63b9f50e41de504e
Noeuclides <euclidesnoeuclides@gmail.com>
2019-06-15 23:57:39 +0000

    insertion sort list, node lost

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,19 +6,13 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *auxp, *auxn, *x, *aux2;
+	listint_t *auxp, *auxn, *x;
 
-	aux2 = *list;
-
-
-	while(aux2->next)
-	{
-
-		auxp = *list;
-		x = (*list)->next;
+	auxp = *list;
+	x = (*list)->next;
+	auxn = x->next;
 		while(x->next)
 		{
-			auxn = x->next;
 			if(x->n < auxp->n)
 			{
 				auxp->next = auxn;
@@ -26,18 +20,29 @@ void insertion_sort_list(listint_t **list)
 				x->prev = auxp->prev;
 				auxp->prev = x;
 				x->next = auxp;
+				printf("auxp->n %d -  x->n %d - auxn->n %d\n", auxp->n, x->n, auxn->n);
 				if (auxp->prev->prev == NULL)
 				{
 					*list = auxp->prev;
+					x = auxn;
+					auxn = x->next;
+					printf("if adentro\n");
 				}
-				/*printf("auxp->n %d -  x->n %d - auxn->n %d\n", auxp->n, x->n, auxn->n);*/
-				print_list(*list);
-				printf("\n");
+				else
+				{
+					auxp = x->prev;
+					auxn = x->next;
+					printf("else adentro\n");
+				}
+				printf("auxp->n %d -  x->n %d - auxn->n %d\n", auxp->n, x->n, auxn->n);
 			}
-			x = x->next;
-			auxp = x->prev;
+			else
+			{
+				x = auxn;
+				auxn = auxn->next;
+				auxp = auxp->next;
+				printf("else afuera\n");
+			}
+			print_list(*list);
 		}
-		aux2 = aux2->next;
-
-	}
 }
