47e955f0e0f9cc714ce608babe3222f39e9fb78e
arleybri18 <arleybri18@gmail.com>
2019-06-15 00:51:34 +0000

    changes for insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * insertion_sort_list -
  *
@@ -7,26 +6,38 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *auxp, *auxn, *x;
+	listint_t *auxp, *auxn, *x, *aux2;
 
-	auxp = *list;
-	x = (*list)->next;
-	while(x)
+	aux2 = *list;
+
+
+	while(aux2->next)
 	{
-		auxn = x->next;
-		if(x->n < auxp->n)
+
+		auxp = *list;
+		x = (*list)->next;
+		while(x->next)
 		{
-			auxp->next = auxn;
-			auxn->prev = auxp;
-			x->prev = auxp->prev;
-			auxp->prev = x->next;
-			x->next = auxp;
-			if (auxp->prev->prev == NULL)
+			auxn = x->next;
+			if(x->n < auxp->n)
 			{
-				*list = auxp->prev;
+				auxp->next = auxn;
+				auxn->prev = auxp;
+				x->prev = auxp->prev;
+				auxp->prev = x;
+				x->next = auxp;
+				if (auxp->prev->prev == NULL)
+				{
+					*list = auxp->prev;
+				}
+				/*printf("auxp->n %d -  x->n %d - auxn->n %d\n", auxp->n, x->n, auxn->n);*/
+				print_list(*list);
+				printf("\n");
 			}
+			x = x->next;
+			auxp = x->prev;
 		}
-		x = x->next;
-		auxp = x->prev;
+		aux2 = aux2->next;
+
 	}
 }
