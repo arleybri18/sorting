ebdd002c955c6b1347eb9baa76f8829ccad2d564
arleybri18 <arleybri18@gmail.com>
2019-06-17 15:43:51 +0000

    implement quick sort

--- sort.h
+++ sort.h
@@ -24,6 +24,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
+int partition(int *array, int first, int last);
+void quick_sort_r(int *array, int first, int last);
 
 #endif /* SORT_H */
